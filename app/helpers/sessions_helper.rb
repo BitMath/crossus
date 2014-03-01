c8b1edd82384628a30208faf9812723ae23ad44d
gadfly86 <gadfly86@gmail.com>
2014-03-01 17:08:27 +0700

    Finish sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,28 +1,30 @@
 module SessionsHelper
   
-  def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
-    self.current_user = user
-  end
+   def sign_in(user)
+     remember_token = User.new_remember_token
+     cookies.permanent[:remember_token] = remember_token
+     user.update_attribute(:remember_token, User.encrypt(remember_token))
+     self.current_user = user
+   end
 
-  def signed_in?
-    !current_user.nil?
-  end
+   def signed_in?
+     !current_user.nil?
+   end
   
  
-  def current_user
-   @current_user ||= user_from_remember_token
-  end
-  
-private
+   def current_user=(user)
+    @current_user = user
+   end
 
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
+   def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+   end
 
-    def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
-    end
+   def sign_out
+     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
+     cookies.delete(:remember_token)
+     self.current_user = nil
+   end
+   
 end
